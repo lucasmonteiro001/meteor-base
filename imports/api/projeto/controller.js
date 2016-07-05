df02576597405cf688b3cfbcb0ca5d6c64fa7f3d
viniciusponciano <viniciusponciano@dcc.ufmg.br>
2016-07-05 10:01:24 -0300

    Adição do CRUD projeto e alterações no formGenerator para exibir dados do tipo vetor

---
+++ imports/api/projeto/controller.js
@@ -0,0 +1,13 @@
+import { CollectionProjetos } from './collection';
+import { ControllerBase } from '../reuse/controllerBase';
+
+class ControllerProjetos extends ControllerBase {
+
+}
+
+export const projetosController = new ControllerProjetos(CollectionProjetos);
+
+// É possível definir neste local as regras de dispobinilizaçao/publicação do conteúdo da collection
+// Por exemplo, só exibe os clientes que torcem para o glorioso Atlético-MG:
+// CtrlCliente.setFilter({time: 'Atlético-MG'})
+
